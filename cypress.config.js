e7381f426d2c03713a56bad7711da91ecc722016
Alena <alenakhan093@gmail.com>
2023-05-29 00:07:31 +0900

    add allure report, change configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 7000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
   },
 });
