7807e255f4c2033c1a98f67aaefdbf7933e3690c
Alena <alenakhan093@gmail.com>
2023-06-04 18:11:34 +0900

    add logout test

--- cypress/pageObjects/SecureAreaPage.js
+++ cypress/pageObjects/SecureAreaPage.js
@@ -1,7 +1,14 @@
+import LoginPage from "./LoginPage";
+
 class SecureAreaPage {
     getSecureAreaPageSuccessMessage = () => cy.get('#flash[class="flash success"]');
     getSecureAreaPageHeader = () => cy.get('h2');
+    getLogoutButton = () => cy.get('a.button[href="/logout"]');
 
-    
+
+    clickLogoutButton() {
+        this.getLogoutButton().click();
+        return new LoginPage;
+    }
 }
 export default SecureAreaPage;
